bc5c75772cc08ccd58824186060fa16f35d8f107
yuuaakiba <yuuaakiba@gmail.com>
2025-02-27 17:38:32 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -29,6 +29,72 @@ $(function($){
         })
     })
 
+    particlesJS('particles-js', {
+        particles: {
+            "number": {
+            "value": 12,
+            "density": {
+                "enable": true,
+                "value_area": 100
+                }
+            },
+
+            "shape":{
+                "type": "circle",
+                "stroke": {
+                    "width": 0, 
+                    "color": "#ffcc00" 
+                }
+            },
+
+            "color": {
+                "value": "#ffffff" 
+            },
+
+            "opacity": {
+                "value": 1, 
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            
+            "size":{
+                "value": 3,
+                "random":true,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+
+            "line_linked":{
+                "enable": true,
+                "distance": 150,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 1
+            },
+
+            "move":{
+                "speed": 4,
+                "straight": false,
+                "direction": "none",
+                "out_mode": "out"
+            }
+
+        },
+
+        "retina_detect": true,
+        "resize": true
+    });
+    
+
     $('.slider').slick({
         arrows: true,
         autoplay: false,
